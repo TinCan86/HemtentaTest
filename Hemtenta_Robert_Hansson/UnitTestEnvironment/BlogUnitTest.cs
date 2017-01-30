142914b8609f7cc83254b2535e33ef75339d7fe9
Robert Hansson <roberthanssonr@gmail.com>
2017-01-30 14:21:23 +0100

    Added UnitTestEnvironment project, added reference for Xunit

---
+++ Hemtenta_Robert_Hansson/UnitTestEnvironment/BlogUnitTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using HemtentaTdd2017.blog;
+using Xunit;
+
+namespace UnitTestEnvironment
+{
+    public class BlogUnitTest
+    {
+        IBlog bg;
+
+        public BlogUnitTest()
+        {
+            bg = new Blog();
+        }
+
+
+        [Theory]
+        //[InlineData("B", 26)]       
+        public void EnMetod()
+        {
+            //Act
+
+
+            //Arrange
+
+
+            //Assert
+        }
+
+        //När man inte har parametrar att tänka på. Kan vara tex ett exception
+        [Fact]
+        public void EnTillMetod()
+        {
+
+        }
+
+        [Fact]
+        public void TestMethod1()
+        {
+            Assert.Equal(4, Add(2, 2));
+
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+    }
+}
